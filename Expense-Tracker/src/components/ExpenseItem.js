326ab1e5afffe3784d6654fc96da0db98f7d2d73
kishansudani <48625369+kishansudani@users.noreply.github.com>
2022-09-18 20:26:55 +0530

    Expense-Tracker

---
+++ Expense-Tracker/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+export default function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$500</div>
+      </div>
+    </div>
+  );
+}
