07240916ff11fddc819decd7d8fc90d1187abbc6
kishansudani <48625369+kishansudani@users.noreply.github.com>
2023-06-04 14:43:29 +0530

    code with harry

---
+++ Code-With-Harry/textutils/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        pariatur ducimus fugit vero maiores. Numquam nesciunt hic excepturi
+        sequi illo alias tempora, est impedit illum consequatur sunt, aperiam
+        minima vel?
+      </p>
+    </>
+  );
+}
+
+export default App;
